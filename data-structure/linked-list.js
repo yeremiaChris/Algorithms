0805d1acc1fd828e812348e99d90f7b9a9b59792
Yeremia Chris Saragi <67268574+yeremiaChris@users.noreply.github.com>
2025-04-02 00:20:27 +0700

    feat: add linked list data structure (#2)
    Co-authored-by: YEREMIA CHRIS SARAGI <yeremiachrissaragi@YEREMIAs-MacBook-Air.local>

---
+++ data-structure/linked-list.js
@@ -0,0 +1,137 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+  // Big-O - O(1) - Contant Time Complexity
+  prepend(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+    this.size++;
+  }
+
+  //   Big-O - Linear - O(n)
+  append(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      let prev = this.head;
+      while (prev.next) {
+        prev = prev.next;
+      }
+      prev.next = node;
+    }
+    this.size++;
+  }
+
+  insert(value, index) {
+    if (index < 0 || index > this.size) return;
+    if (index === 0) {
+      this.prepend(value);
+    } else {
+      const node = new Node(value);
+      let prev = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        prev = prev.next;
+      }
+      node.next = prev.next;
+      prev.next = node;
+      this.size++;
+    }
+  }
+
+  removeFrom(index) {
+    if (index < 0 || index > this.size) return;
+    let removeNode;
+    if (index === 0) {
+      removeNode = this.head;
+      this.head = this.head.next;
+    } else {
+      let prev = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        prev = prev.next;
+      }
+      removeNode = prev.next;
+      prev.next = removeNode.next;
+    }
+    this.size--;
+    return removeNode;
+  }
+
+  removeValue(value) {
+    if (this.isEmpty()) return null;
+    if (this.head.value === value) {
+      this.head = this.head.prev;
+      this.size--;
+      return value;
+    } else {
+      let prev = this.head;
+      while (prev.next && prev.next.value !== value) {
+        prev.value = prev.next;
+      }
+      if (prev.next) {
+        const removedNode = prev.next;
+        prev.next = removedNode.next;
+        this.size--;
+        return removedNode;
+      }
+      return null;
+    }
+  }
+
+  print() {
+    if (this.isEmpty()) {
+      console.log("the list is empty");
+    } else {
+      let curr = this.head;
+      let listValues = "";
+      while (curr) {
+        listValues += `${curr.value} `;
+        curr = curr.next;
+      }
+      console.log(listValues);
+    }
+  }
+}
+const list = new LinkedList();
+
+// list.print();
+// list.append(10);
+// list.print();
+// list.append(20);
+// list.append(30);
+// list.print();
+
+list.insert(10, 0);
+list.print();
+list.insert(20, 0);
+list.print();
+list.insert(30, 1);
+list.print();
+list.insert(40, 2);
+list.print();
+list.removeFrom(0);
+list.print();
+list.removeValue(40);
+list.print();
---
+++ data-structure/linked-list.md
@@ -0,0 +1,20 @@
+# Linked List
+
+A linked list is a linear data structure that includes the series of connected node
+Each node constist of a data value and a pointer that points to the next node
+The list elements can be easily inserted or removed without reallocation or reorganization of the entire structure
+Random access of elements is not feasible and accessing an element has linear time complexity
+The linked list data structure supports three main operations
+
+1. Insertion - to add an element at the beginning, end or at a given index in the list
+2. Deletion - to remove an item given index or value
+3. Search - to find an element given its value
+
+# Linked list visualization
+
+![alt text](image-5.png)
+
+# Linked List Usage
+
+All applications of both stacks and queues are applications of linked lists
+Image Viewer
